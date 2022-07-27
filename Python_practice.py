a5a46da47d5f676ae02641eb4b98513d6607b747
McKaye Peterson <mckayepeterson@MacBook-Air.local>
2022-07-27 16:05:40 -0600

    Adding the election audit code.

---
+++ Python_practice.py
@@ -0,0 +1,9 @@
+my_list = ["Peanut Butter", "Kerrygold", "Coffee", "Eggs"]
+for item in my_list: # item is a temporary variable right now
+    #run through the for loop for as many number of items there are
+    print(item)
+my_list[my_list.index("Kerrygold")] = "Kerrygold Unsalted"
+my_list.append["Flour"]
+my_list.append["Brown Sugar"]
+# for any iterable you can insert that into a list
+print(x) for x in my_list
